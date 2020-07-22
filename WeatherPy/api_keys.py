ef4378c828878e35d83de3cb20c564d9a61bc930
unknown <tony.gardella5@gmail.com>
2020-07-22 15:13:26 -0500

    City data imported into lists

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "2b078b8a93fdc79bd08191d166286696"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyD4hkC7rkkAYV8D4U5XXAfPRYCjKtS4MiE"
